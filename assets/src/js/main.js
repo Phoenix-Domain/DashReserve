05362f770025647bed9cd7cb4990455083ad08ee
Batubo Victory <victorybatubo76@gmail.com>
2025-07-11 18:24:49 +0100

    Declare input values

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -4,12 +4,15 @@ function MakeUser(name,service,date){
   this.service = service
 }
 const clientName = document.querySelector('#clientName');
-const servicetype = document.querySelector('#serviceType');
+const serviceType = document.querySelector('#serviceType');
 const bookingDate = document.querySelector('#bookingDate');
 const addBookingBtn = document.querySelector('#addBookingBtn');
 const bookingList = document.querySelector('#bookingList');
 
 addBookingBtn.addEventListener('click', e => {
   e.preventDefault();
+  const nameValue = clientName.value;
+  const serviceValue = serviceType.value;
+  const dateValue = bookingDate.value;
 
 })
