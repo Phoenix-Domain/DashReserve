9139741f7b02a45fd3f64c27a3a79f9e3f1a48ce
Batubo Victory <victorybatubo76@gmail.com>
2025-07-11 18:34:58 +0100

    Add displayBooking function

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,8 +1,9 @@
-function MakeUser(name,service,date){
+class MakeUser{
+  constructor(name,service,date){
   this.name = name
   this.date = date
   this.service = service
-}
+}}
 const clientName = document.querySelector('#clientName');
 const serviceType = document.querySelector('#serviceType');
 const bookingDate = document.querySelector('#bookingDate');
@@ -15,4 +16,15 @@ addBookingBtn.addEventListener('click', e => {
   const serviceValue = serviceType.value;
   const dateValue = bookingDate.value;
 
+  let user = new MakeUser(nameValue,serviceValue,dateValue);
+  displayBooking(user);
 })
+
+const displayBooking = x => {
+  const list = document.createElement('li');
+  const delBtn = document.createElement('button');
+  delBtn.textContent = "Delete";
+  list.textContent = `${x.name} booked ${x.service} on ${x.date}`;
+  bookingList.append(list);
+  bookingList.append(delBtn);
+}
