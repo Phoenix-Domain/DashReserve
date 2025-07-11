699b38d5cfa13f885869770f0e16d8ec77e01c23
Batubo Victory <victorybatubo76@gmail.com>
2025-07-11 18:16:29 +0100

    Make user object

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,5 +1,15 @@
+function MakeUser = (name,service,date){
+  this.name = name
+  this.date = date
+  this.service = service;
+}
 const clientName = document.querySelector('#clientName');
 const servicetype = document.querySelector('#serviceType');
 const bookingDate = document.querySelector('#bookingDate');
 const addBookingBtn = document.querySelector('#addBookingBtn');
 const bookingList = document.querySelector('#bookingList');
+
+addBookingBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+})
