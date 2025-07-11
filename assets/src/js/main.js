d5e7c69219be507e0c411d46788b01816e8391c1
Batubo Victory <victorybatubo76@gmail.com>
2025-07-11 11:58:14 +0100

    Declare DOM variables

---
+++ assets/src/js/main.js
@@ -0,0 +1,5 @@
+const clientName = document.querySelector('#clientName');
+const servicetype = document.querySelector('#serviceType');
+const bookingDate = document.querySelector('#bookingDate');
+const addBookingBtn = document.querySelector('#addBookingBtn');
+const bookingList = document.querySelector('#bookingList');
